a79f05f0f82bf88855c476cda65cce2406a6e5c5
chrislzm <me@chrisleung.com>
2017-03-14 20:53:28 -0700

    Cleaned up code

--- RockPaperScissors/MakeMove.swift
+++ RockPaperScissors/MakeMove.swift
@@ -10,73 +10,64 @@ import UIKit
 
 class MakeMoveViewController: UIViewController {
 
-    let moves = ["rock","paper","scissors"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func moveMade(_ sender:AnyObject) {
-        let button = sender as! UIButton
-        let playerMove = moves[button.tag]
+    @IBAction func playPaper(_ sender:AnyObject) {
+        let playerMove = "paper"
         
-        switch playerMove {
-        case "paper":
-            // Present GameResult viewcontroller in all code as per requirements
-            var controller:GameResultViewController
-            controller = self.storyboard?.instantiateViewController(withIdentifier: "GameResultViewController") as! GameResultViewController
-            let (compMove,result,resultImage) = playGame(playerMove)
-            controller.resultLabelText = result
-            controller.resultImageName = resultImage
-            controller.playerImageName = playerMove
-            controller.compImageName = compMove
-            self.present(controller, animated: true, completion: nil)
-        case "rock":
-            // Present GameResult viewcontroller via segue by identifier as per requirements
-            performSegue(withIdentifier: "hitRock", sender: self)
-        default: break
-        }
+        // Present GameResult viewcontroller in all code as per requirements
+        var controller:GameResultViewController
+        controller = self.storyboard?.instantiateViewController(withIdentifier: "GameResultViewController") as! GameResultViewController
+        playGamePushResults(playerMove,controller)
+        self.present(controller, animated: true, completion: nil)
     }
 
-    func playGame(_ playerMove:String) -> (String,String,String) {
+    @IBAction func playRock(_ sender:AnyObject) {
+        performSegue(withIdentifier: "playRock", sender: self)
+    }
+
+    func playGamePushResults(_ playerMove:String,_ controller:GameResultViewController) {
+        let moves = ["rock","paper","scissors"]
         let compMove = moves[Int(arc4random() % 3)]
         let (result,resultImage) = returnResultAndImage(playerMove, compMove)
-        return (compMove,result,resultImage)
+        controller.resultLabelText = result
+        controller.resultImageName = resultImage
+        controller.playerImageName = playerMove
+        controller.compImageName = compMove
     }
     
     func returnResultAndImage (_ playerMove:String, _ compMove:String) -> (String,String) {
-    if playerMove == "rock" && compMove == "scissors" {
-        return ("Player","RockCrushesScissors")
-    } else if playerMove == "scissors" && compMove == "paper" {
-        return ("Player","ScissorsCutPaper")
-    } else if playerMove == "paper" && compMove == "rock" {
+        if playerMove == "rock" && compMove == "scissors" {
+            return ("Player","RockCrushesScissors")
+        } else if playerMove == "scissors" && compMove == "paper" {
+            return ("Player","ScissorsCutPaper")
+        } else if playerMove == "paper" && compMove == "rock" {
             return ("Player","PaperCoversRock")
-    } else if compMove == "rock" && playerMove == "scissors" {
-        return ("Computer","RockCrushesScissors")
-    } else if compMove == "scissors" && playerMove == "paper" {
-        return ("Computer","ScissorsCutPaper")
-    } else if compMove == "paper" && playerMove == "rock" {
-        return ("Computer","PaperCoversRock")
-    } else {
-        return ("Tie","itsATie")
+        } else if compMove == "rock" && playerMove == "scissors" {
+            return ("Computer","RockCrushesScissors")
+        } else if compMove == "scissors" && playerMove == "paper" {
+            return ("Computer","ScissorsCutPaper")
+        } else if compMove == "paper" && playerMove == "rock" {
+            return ("Computer","PaperCoversRock")
+        } else {
+            return ("Tie","itsATie")
         }
     }
     
     // Will only be called if player hits rock or scissors buttons
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         var playerMove:String
-        if segue.identifier == "hitRock" {
+        
+        if segue.identifier == "playRock" {
             playerMove = "rock"
         } else { // Will be scissors
             playerMove = "scissors"
         }
         
         let controller = segue.destination as! GameResultViewController
-        let (compMove,result,resultImage) = playGame(playerMove)
-        controller.resultLabelText = result
-        controller.resultImageName = resultImage
-        controller.playerImageName = playerMove
-        controller.compImageName = compMove
+        playGamePushResults(playerMove,controller)
     }
 }
 
